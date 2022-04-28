ddeede0da89defa1fc1d7845a2a3a6d01eb44bd8
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-04-28 10:50:16 +0300

    feat: 04-copy-directory release

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir, mkdir, copyFile} = require('fs/promises')
 
+const source = path.join(__dirname+'/files/')
+const dest = path.join(source.substring(0,source.length-1)+'-copy/')
+mkdir(dest, {recursive: true})
+
+
+try {
+    readdir(source)
+      .then(items => items.forEach(item => copyFile(source+item, dest+item)))
+  } catch (err) {
+    console.error(err);
+  }
