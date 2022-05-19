8914db2a845ea0ded6f1fd66723f2ae58847c2dd
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-05-19 19:04:22 +0300

    feat: 06-build-page: html templates + css bundle release

---
+++ 06-build-page/index.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir, copyFile } = require('fs/promises')
+let templateFile = ''
+const templatePath = path.join(__dirname, '/template.html');
+const stream = fs.createReadStream(templatePath);
+const resultPath = path.join(__dirname + '/project-dist')
+stream.on('data', (data) => templateFile += data.toString());
+stream.on('end', () => {
+    const regexp = /{{\w{1,}}}/g
+    const matches = templateFile.match(regexp);
+    let counter = 0;
+    matches.forEach(match => {
+        const filename = match.slice(2, -2);
+        const readStream = fs.createReadStream(path.join(__dirname, '/components/', filename + '.html'));
+        readStream.on('data', (data) => {
+            counter++;
+            templateFile = templateFile.replace(match, data.toString())
+        })
+        readStream.on('close', () => {
+            if (counter == matches.length) {
+                const writer = fs.createWriteStream(path.join(resultPath, '/index.html'))
+                writer.write(templateFile)
+                writer.close()
+            }
+        })
+    })
+})
+
+fs.mkdir(resultPath, (err) => {
+    if (err) {
+        return console.error(err);
+    }
+    console.log('Directory created successfully!');
+})
+
+
+
+const source = path.join(__dirname+'/styles/')
+const destination = path.join(__dirname+'/project-dist/')
+const destFile = 'style.css';
+let globalStyles = [];
+const writer2 = fs.createWriteStream(destination+destFile)
+try {
+    readdir(source, { withFileTypes: true })
+        .then(items => {
+            items.forEach(item => {
+                console.log(item)
+                if (!item.isFile()) return;
+                if (path.extname(item.name) !== '.css') return;
+                const stream = fs.createReadStream(source + item.name)
+                stream.on('data', (data) => {
+                    let dat = data.toString();
+                    globalStyles.push(dat);
+                    writer2.write(dat);
+                    stream.close()
+                })
+            })      
+        })
+} catch (error) {
+    throw error;
+}
