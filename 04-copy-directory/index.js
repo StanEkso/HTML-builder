ff438669b93796ccbb932c5f74d5d3f94affda2c
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-05-24 21:13:51 +0300

    fix: 04-copy-directory add delete of old files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,8 +4,17 @@ const { readdir, mkdir, copyFile} = require('fs/promises')
 
 const source = path.join(__dirname+'/files/')
 const dest = path.join(source.substring(0,source.length-1)+'-copy/')
+fs.stat(dest,(err, stat) => {
+  if (!err) {
+    readdir(dest)
+      .then(items => items.forEach(item =>
+      fs.rm(dest+item, (err) => err)))  
+      .then(items =>copyDirectory(source)); 
+  } else {
+    copyDirectory(source)
+  }
+})
 mkdir(dest, {recursive: true})
-
 function copyDirectory(source) {
   readdir(source)
       .then(items => items.forEach(item => {
@@ -20,8 +29,3 @@ function copyDirectory(source) {
         }) ;
       }))
 }
-try {
-   copyDirectory(source)
-  } catch (err) {
-    console.error(err);
-  }
